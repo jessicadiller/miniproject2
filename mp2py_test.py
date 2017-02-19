7862144cb2f6ab1b055b0919976c91fa812ab2b7
Jessica Diller <dillerpickle@gmail.com>
2017-02-19 13:57:13 -0500

    added spring function

--- mp2py_test.py
+++ mp2py_test.py
@@ -5,16 +5,13 @@ at = mp.jtest()
 
 def angle_get():
     measured_angle = at.get_angle()
-    #print "measured angle:"
-    #print measured_angle
+    #print "measured angle: ",measured_angle
     #print "{0:b}".format(measured_angle[0])
     #print "{0:b}".format(measured_angle[1])
     int_angle = int(measured_angle[0]) + int(measured_angle[1])*256
-    #print "measured angle integer:"
-    #print int_angle
+    #print "measured angle integer: ",int_angle
     position = (int_angle - 16167) / 46.59
-    print "position:"
-    print position
+    print "position: ",position
     return position;
 
 #speed_get function
@@ -23,25 +20,19 @@ def torque_get():
     at.toggle_led1()
     #GET CURRENT
     analog = at.get_current()
-    #print "get_current output:"
-    #print analog
+    #print "get_current output: ",analog
     
     #TORQUE CALC
     measuredvout = int(analog[0])+int(analog[1])*256 #formats to integer
-    #print "measured integer:"
-    #print measuredvout
+    #print "measured integer: ",measuredvout
     fraction = (float(measuredvout) / 65535)
-    #print "vout fraction:"
-    #print fraction
+    #print "vout fraction: ",fraction
     normalized = fraction * 3.3
-    #print "vout actual:"
-    #print normalized
+    #print "vout actual: ",normalized
     current = (normalized - 1.6) / 0.75
-    #print "current calculation:"
-    #print current
+    #print "current calculation: ",print current
     real = 4 * current
-    print "real torque:"
-    print real
+    print "real torque:, ",real
     return real;
 
 def pwm_control(realT, idealT, change):
@@ -89,8 +80,7 @@ def wall_control(position):
         control = False
         print "not past threshold"
         at.set_duty_f(0)
-    #print "control:"
-    #print control
+    #print "control: ",control
 
     #pwm_control(torque, ideal, control);
 
@@ -107,15 +97,26 @@ def wall_control(position):
     return;
 
 #spring_control
+def spring_control(position):
+    torque = torque_get()
+    #print "torque: ",torque
+    ideal = position * -3 #FIGURE OUT ANGLE SYSTEM
+    #print "ideal: ",ideal
+    #pwm_control(torque, ideal, control, 1)
+    print "done"
+    return;
+
+
 #damper_control
 #texture_control
 
 #call functions
 #t = 1
 #while t < 1000000000000000000000000:
-while True:
-    position = angle_get()
-    wall_control(position)
+#while True:
+position = angle_get()
+spring_control(position)
+#    wall_control(position)
     #t = t+1
 
 
